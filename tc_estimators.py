88d793d3db643e7ebe285c2e8cf80ea4d3865586
Linear95 <linear95@outlook.com>
2020-10-23 16:56:37 -0400

    update tree like tc

--- tc_estimators.py
+++ tc_estimators.py
@@ -27,7 +27,51 @@ class TotalCorEstimator(nn.Module):
         for i in range(1, num_var):
             outputs.append(self.mi_estimators[i-1].learning_loss(samples[:,:i].flatten(start_dim = 1), samples[:,i]))
         return torch.stack(outputs).mean()
-        
+
+
+class Tree_TC_Estimator(nn.Module):
+    def __init__(self, num_var, dim, hidden_size, mi_est_name = "CLUB"):
+        super(Tree_TC_Estimator, self).__init__()
+        self.num_var = num_var
+        self.mi_est_name = mi_est_name
+        self.hidden_size = hidden_size
+        self.est_index = []
+        self.root = self._build_tree(0, num_var-1)
+        print(self.est_index)
+        estimator_list = [eval(mi_est_name)((mid-l+1)*dim, (r-mid)*dim, int(hidden_size*np.sqrt(r-l))) for (l, mid, r) in self.est_index]
+        #print(estimator_list)
+        # while 
+        #estimator_list.append(eval(mi_est_name)(dim*2, dim*2, int(hidden_size * np.sqrt(2))))
+        self.mi_estimators = nn.ModuleList(estimator_list)
+
+    def _build_tree(self, l, r):
+        if r-l <= 0:
+            return
+        else:
+            mid = (r+l)//2
+            self.est_index.append((l, mid, r))
+            self._build_tree(l, mid)
+            self._build_tree(mid+1, r)
+            return 
+
+    def forward(self, samples):
+        output_list = []
+        for i in range(len(self.est_index)):
+            l, mid, r = self.est_index[i]
+            output_list.append(self.mi_estimators[i](
+                samples[:,l:mid+1].flatten(start_dim=1), 
+                samples[:,mid+1:r+1].flatten(start_dim=1)))
+        #print(mi_output_list)
+        return torch.stack(output_list).sum()
+
+    def learning_loss(self, samples):
+        output_list = []
+        for i in range(len(self.est_index)):
+            l, mid, r = self.est_index[i]
+            output_list.append(self.mi_estimators[i].learning_loss(
+                samples[:,l:mid+1].flatten(start_dim=1), 
+                samples[:,mid+1:r+1].flatten(start_dim=1)))
+        return torch.stack(output_list).sum()
 
 
 class TreeTC4Estimator(nn.Module):
@@ -44,7 +88,7 @@ class TreeTC4Estimator(nn.Module):
         return mi_1 + mi_2 + mi_3
 
     def learning_loss(self, samples):
-        loss_1 = self.mi_estimators[2](samples[:,2:].flatten(start_dim=1), samples[:, :2].flatten(start_dim=1))
-        loss_2 = self.mi_estimators[0](samples[:,0], samples[:,1])
-        loss_3 = self.mi_estimators[1](samples[:,2], samples[:,3])
+        loss_1 = self.mi_estimators[2].learning_loss(samples[:,2:].flatten(start_dim=1), samples[:, :2].flatten(start_dim=1))
+        loss_2 = self.mi_estimators[0].learning_loss(samples[:,0], samples[:,1])
+        loss_3 = self.mi_estimators[1].learning_loss(samples[:,2], samples[:,3])
         return (loss_1+loss_2+loss_3)/3.
